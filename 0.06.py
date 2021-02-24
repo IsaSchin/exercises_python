79bad0e1ddd0c8f046ec9d78a0c16c5b21551a01
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2021-02-24 14:42:07 -0300

    Update 0.06.py

--- 0.06.py
+++ 0.06.py
@@ -1,6 +1,11 @@
-print ("Hello World")
+print ("Hello World") # pode remover essa linha
+
 num1 = float(input ("Digite um número: "))
 num2 = float(input("Digite um segundo número: "))
 num3 = float (input("Digite um terceiro número: "))
+
+# está correto. Porem, tente me mostrar uma solução com ifs e elses
+# para treinar a logica :)
+
 res = max(num1, num2, num3)
 print (f"o maior número digitado foi: {res}")
