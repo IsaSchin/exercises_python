ac8c2d28e73258b330de17166dc248c24aa6cd97
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2021-02-24 14:43:30 -0300

    Update 0.01.py

--- 0.01.py
+++ 0.01.py
@@ -1,5 +1,9 @@
-print ("Hello World")
+print ("Hello World") # pode remover essa linha (ou imprima o titulo do exercicio pelo menos xD)
 num1 = float(input ("Digite um número: "))
 num2 = float(input("Digite um segundo número: "))
+
+# está correto. Porem tente me apresentar uma solução usando ifs e elses
+# para treinar a logica :)
+
 res = max(num1, num2)
 print (f"o maior número digitado foi: {res}")
