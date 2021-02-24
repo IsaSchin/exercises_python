9780e0fcc47ed90813f255ce3a305e1fede02313
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2021-02-24 14:14:06 -0300

    Update 0.03.py

--- 0.03.py
+++ 0.03.py
@@ -1,4 +1,4 @@
-print ("Hello World")
+print ("Hello World") #pode remover essa linha
 genero = input("Digite seu genero (F / M / não quero definir): ")
 
 if genero == "F":
@@ -11,3 +11,16 @@ elif genero == "m":
     print ("Sexo Masculino")
 else:
     print ("Não quero definir")
+
+# => está correto.. só uma dica de otimização:
+#
+# essas 4 condições podem ser alteradas pra 2:
+#
+# if genero == "F" or genero == "f":
+#    print ("Sexo Feminino")
+#
+# o mesmo pode ser feito com os outros 2
+
+# outra coisa:
+# faltou uma parte do exercicio em que ele pede pra vc dizer que qualquer outro valor digitado não é valido.
+# no caso, pode ter 3 inputs (m/f/n) mas caso for digitado algum alem desses, a mensagem tem que ser "opcao invalida"
