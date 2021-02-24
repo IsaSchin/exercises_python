1ec316d220018cda348428d036f37554cb617bc8
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2021-02-24 14:38:43 -0300

    Update 0.05.py

--- 0.05.py
+++ 0.05.py
@@ -1,12 +1,16 @@
-print ("Hello World")
+print ("Hello World") # pode remover essa linha
+
 a = float(input("Nota parcial 1: "))
 b = float(input("Nota parcial 2: "))
 
+# nota 13 e -4, por exemplo, não existem. Faltou a verificação do input
+# a nota inserida tem que ser positiva e de valor entre 0 e 10!
+
 media = (a + b) / 2
 
-if media >= 7 and media < 10:
+if media >= 7 and media < 10: # correto
     print (f"Sua média é {media} e você foi APROVADO!)")
-elif media >= 10:
+elif media >= 10: # <--- a media nunca deve ser maior que 10
     print (f"Sua média é 10 e você foi APROVADO COM DISTINÇÃO")
-else:
+else: # correto
     print (f"Sua média é {media} e você foi REPROVADO")
